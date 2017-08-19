ee377be9c088802d63f129e2203ffb330fdf509b
ruanhao <1324587720@qq.com>
2017-08-19 11:50:20 +0800

    some method of files

---
+++ method.py
@@ -0,0 +1,9 @@
+import lxml.html
+
+def parse_form(html):
+    tree=lxml.html.fromstring(html)
+    data={}
+    for e in tree.cssselect('form input'):
+        if e.get('name'):
+            data[e.get('name')]=e.get('value')
+    return data
