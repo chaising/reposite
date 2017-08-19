bf1cc03404538062b7512cfc633b007b9eee6e63
ruanhao <1324587720@qq.com>
2017-08-19 15:06:05 +0800

    Include MongoQueu and logging module

--- classmethod.py
+++ classmethod.py
@@ -1,7 +1,11 @@
 import urllib2
-from pymongo import MongoClient
+from pymongo import MongoClient,errors
 import random
 import urlparse
+import logging
+from datetime import datetime , timedelta
+
+logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s] (%(threadName)s) %(message)s",)
 
 class Download:
     def __init__(self,throttle=None,proxies=None,num_retries,headers=None,user_agent='wspa',cache=None):
@@ -30,6 +34,7 @@ class Download:
     
     
     def download(self,url,num_retries=2):
+        logging.debug("Downloading...%s",url)
         headers=self.headers or {}
         if self.user_agent:
             headers['User-agent']=self.user_agent
@@ -52,6 +57,40 @@ class Download:
         return {'html':html,'code':code}
     
     
-                
-        
+class MongoQueue:
+    OUTSTANDING,PROCESSING,COMPLETED=range(3)
+    def __init__(self,client=None,timeout=300):
+        self.client=MongoClient("mongodb://localhost:27017/") if client is None else client
+        self.db=self.client.datas 
+        self.timeout=timeout
     
+    def __nonzero__(self):
+        record=self.db.coll.find_one({'status':{'$ne':self.COMPLETED}})
+        return True if record else False
+    
+    def push(self,url):
+        try:
+            self.db.coll.insert({'_id':url,'status':self.OUTSTANDING})
+        except errors.DuplicateKeyError:
+            pass  
+    
+    def pop(self):
+        record=self.db.coll.find_and_modify(query={'status':self.OUTSTANDING},update={'$set':{'status':self.PROCESSING,'timestamp':datetime.now()}})
+        if record:
+            return record['_id']
+        else:
+            self.repair()
+            raise KeyError()    #This is details that show the last uncompleted url in mongodb
+    
+    def complete(self,url):
+        self.db.coll.update({'_id':url},{'$set':{'status':self.COMPLETED}})
+        
+    def repair(self):
+        
+        record=self.db.coll.find_and_modify(query={'timstamp':{'$lt':datetime.now()-timedelta(seconds=self.timeout)},'status':{'$ne':self.COMPLETED}},
+                                            update={'$set':{'status':self.OUTSTANDING}})
+        if record:
+            logging.debug('Released:%s',record['_id'])
+            
+
+
