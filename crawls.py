466ed8a76d513559db1e90765b3fdc81583f3b85
ruanhao <1324587720@qq.com>
2017-08-19 11:24:05 +0800

    add response

--- crawls.py
+++ crawls.py
@@ -1,5 +1,9 @@
 import urllib,urllib2
-LOGIN_URL='http://example.webscraping.com/user/login'
+LOGIN_URL=''
 LOGIN_EMAIL='example@webscraping.com'
 LOGIN_PASSWORD='example'
 data={'email':LOGIN_EMAIL,'password':LOGIN_PASSWORD}
+encoded_data=urllib.urlencode(data)
+request=urllib2.Request(LOGIN_URL,encoded_data)
+response=urllib2.urlopen(request)
+response.geturl()
