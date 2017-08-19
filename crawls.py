bb8a0c4da8029c9a082be810b300025aee727480
ruanhao <1324587720@qq.com>
2017-08-19 11:12:29 +0800

    add distributed

--- crawls.py
+++ crawls.py
@@ -1,3 +1,5 @@
 import urllib,urllib2
 LOGIN_URL='http://example.webscraping.com/user/login'
 LOGIN_EMAIL='example@webscraping.com'
+LOGIN_PASSWORD='example'
+data={'email':LOGIN_EMAIL,'password':LOGIN_PASSWORD}
